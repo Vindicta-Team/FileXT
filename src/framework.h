113f284f54ae1bfc4ee6338e52a80df113c4147d
= <=>
2020-07-17 17:26:35 +0200

    check for linux and windows

--- src/framework.h
+++ src/framework.h
@@ -2,11 +2,14 @@
 
 #ifdef _WIN32
 #ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#endif
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#endif WIN32_LEAN_AND_MEAN
 // Windows Header Files
 #include <windows.h>
-#endif
+#endif _WIN32
+
+#ifdef linux
+#endif linux
 
 // STD header files
 #include <string>
