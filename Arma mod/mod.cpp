e2059fafce0ce753c606a638c9f494cf07ac37ed
= <=>
2020-07-17 17:22:38 +0200

    align lines

--- Arma mod/mod.cpp
+++ Arma mod/mod.cpp
@@ -1,10 +1,10 @@
-name		= "filext";								// Name of your mod
-picture 	= ""; 		// Picture displayed from the expansions menu. Optimal size is 2048x1024
-logoSmall	= "";	// Display next to the item added by the mod
-logo		= "";			// Logo displayed in the main menu
-logoOver	= "";		// When the mouse is over, in the main menu
-action		= "https://github.com/Vindicta-Team/filext";			// Website URL, that can accessed from the expansions menu
-tooltipOwned = "filext";								// Tool tip displayed when the mouse is left over, in the main menu
+name         = "filext";                                   // Name of your mod
+picture      = "";                                         // Picture displayed from the expansions menu. Optimal size is 2048x1024
+logoSmall    = "";                                         // Display next to the item added by the mod
+logo         = "";                                         // Logo displayed in the main menu
+logoOver     = "";                                         // When the mouse is over, in the main menu
+action       = "https://github.com/Vindicta-Team/filext";  // Website URL, that can accessed from the expansions menu
+tooltipOwned = "filext";                                   // Tool tip displayed when the mouse is left over, in the main menu
 
 // Color used for DLC stripes and backgrounds (RGBA)
 dlcColor[] =
@@ -15,6 +15,6 @@ dlcColor[] =
 	1
 };
 // Overview text, displayed from the extension menu
-overview = "Extension for structured file I/O operations.";
-hideName	= 0;	// Hide the extension name
-hidePicture	= 0;	// Hide the extension menu
+overview    = "Extension for structured file I/O operations.";
+hideName    = 0; // Hide the extension name
+hidePicture = 0; // Hide the extension menu
